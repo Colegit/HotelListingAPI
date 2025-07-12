4d7831c52969c9bb4e95bf4db0680832a7205814
Colegit <colechapman1993@gmail.com>
2025-07-11 19:30:44 -0700

    DB: Setup of the db context and the connection string to sql server so we are ready to rock and roll like its 1999

--- HotelListing API/Models/HotelListingDbContext.cs
+++ HotelListing API/Models/HotelListingDbContext.cs
@@ -9,6 +9,9 @@ namespace HotelListing_API.Models
             
         }
 
+        //by defining a DbSet, you are instructing a new table to be built, with the title, and attributes based on your data model
+        //entityframework based on your predefined primary/foreign keys in your data model, entity framework will automatically build that into the generated tables
         public DbSet<Country> Countries { get; set; }
+        public DbSet<Hotel> Hotels { get; set; }
     }
 }
